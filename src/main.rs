09c5d74acfebc2b0d66ab0c48e11e1b5fad29ada
Chris Farmer <45003569+ign3u5@users.noreply.github.com>
2021-08-15 19:07:06 +0100

    Next step: The BindGroup

--- src/main.rs
+++ src/main.rs
@@ -150,6 +150,62 @@ impl State {
 
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
+        let diffuse_bytes = include_bytes!("happy-tree.png");
+        let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+        let diffuse_rgba = diffuse_image.as_bgra8().unwrap();
+
+        use image::GenericImageView;
+        let dimensions = diffuse_image.dimensions();
+
+        let texture_size = wgpu::Extent3d {
+            width: dimensions.0,
+            height: dimensions.1,
+            depth_or_array_layers: 1,
+        };
+
+        let diffuse_texture = device.create_texture(
+          &wgpu::TextureDescriptor {
+              size: texture_size,
+              mip_level_count: 1,
+              sample_count: 1,
+              dimension: wgpu::TextureDimension::D2,
+              format: wgpu::TextureFormat::Rgba8UnormSrgb,
+              usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
+              label: Some("diffuse_texture"),
+          }  
+        );
+
+        queue.write_texture(
+            wgpu::ImageCopyTexture {
+                texture: &diffuse_texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            }, 
+            &diffuse_rgba,
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: std::num::NonZeroU32::new(4 * dimensions.0),
+                rows_per_image: std::num::NonZeroU32::new(dimensions.1),
+            }, 
+            texture_size,
+        );
+
+        let diffuse_texture_view = diffuse_texture.create_view(
+            &wgpu::TextureViewDescriptor::default()
+        );
+
+        let diffuce_sampler = device.create_sampler(
+            &wgpu::SamplerDescriptor {
+                address_mode_u: wgpu::AddressMode::ClampToEdge,
+                address_mode_v: wgpu::AddressMode::ClampToEdge,
+                address_mode_w: wgpu::AddressMode::ClampToEdge,
+                mag_filter: wgpu::FilterMode::Linear,
+                min_filter: wgpu::FilterMode::Nearest,
+                mipmap_filter: wgpu::FilterMode::Nearest,
+                ..Default::default()
+            }
+        );
+
         let clear_color = wgpu::Color::BLACK;
 
         let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
